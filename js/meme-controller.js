b41341f02f40ce06808c310584b3e7fc8d0dd78f
yandimant1993 <yan1993dimant@gmail.com>
2025-07-16 18:41:40 +0200

    Create canvas and change image when clicked

--- js/meme-controller.js
+++ js/meme-controller.js
@@ -1,46 +1,20 @@
 'use strict'
 
 const gElCanvas = document.querySelector('.canvas-picture')
-// const gCtx = gElCanvas.getContext('2d')
+const gCtx = gElCanvas.getContext('2d')
 
-function onInit() {
-    renderGallery()
+
+
+function renderMeme(imgUrl) {
+    const img = new Image()
+    img.src = imgUrl
+    img.onload = () => drawImg(img)
 }
 
-function renderGallery() {
-    var strHtml = ''
-    const imgEl = document.querySelector('.gallery')
-    for (var i = 0; i < 18; i++) {
-        strHtml += `<img src="${gImgs[i].url}">`
-    }
-    imgEl.innerHTML = strHtml
+function drawImg(img) {
+    gCtx.drawImage(img, 0, 0, gElCanvas.width, gElCanvas.height)
 }
 
-
-
-
-
-
-
-
-// function () {
-//     const imagesUrl = []
-//     for (var i = 0; i < 12; i++) {
-//         imagesUrl[i] = gImgs[i].url
-//     }
-
-//     imagesUrl.forEach((imgUrl, idx) => {
-//         const img = new Image()
-//         img.src = imgUrl
-//         img.onload = () => {
-//             drawImg(img, idx, imagesUrl.length)
-//         }
-//     })
-// }
-
-
-// function drawImg(img, idx, totalPictures) {
-//     const picWidth = gElCanvas.width / totalPictures
-//     const dist = picWidth * idx
-//     gCtx.drawImage(img, dist, 0, picWidth, picWidth)
-// }
+function setImage(imgUrl) {
+    renderMeme(imgUrl)
+}
