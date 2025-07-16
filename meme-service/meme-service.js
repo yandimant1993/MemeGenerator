014913e477bc374c23acaa7f051261ddc8dcd017
yandimant1993 <yan1993dimant@gmail.com>
2025-07-16 10:59:38 +0200

    Create basic files and data base

---
+++ meme-service/meme-service.js
@@ -0,0 +1,22 @@
+'use strict'
+
+
+var gImgs = [{
+    id: 1
+    , url: 'img/1.jpg'
+    , keyWords: ['funny', 'cat']
+}]
+
+var gMeme = {
+    selectedImgId: 5,
+    selectedLineIdx: 0,
+    lines: [
+        {
+            txt: 'i sometimes eat Falafel',
+            size: 20,
+            color: red
+        }
+    ]
+}
+
+var gKeyWordSearchCountMap = { 'funny': 12, 'cat': 16, 'baby': 2 }
