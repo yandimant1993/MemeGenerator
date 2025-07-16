1d7421fddfafd08710f402e00200f223e6498210
yandimant1993 <yan1993dimant@gmail.com>
2025-07-16 12:52:47 +0200

    Create canvas

--- meme-service/meme-service.js
+++ meme-service/meme-service.js
@@ -1,11 +1,37 @@
 'use strict'
 
 
-var gImgs = [{
-    id: 1
-    , url: 'img/1.jpg'
-    , keyWords: ['funny', 'cat']
-}]
+var gImgs = [
+    {
+        id: 1
+        , url: 'img/1.jpg'
+        , keyWords: ['funny', 'cat']
+    }
+    ,
+    {
+        id: 2
+        , url: 'img/2.jpg'
+        , keyWords: ['funny', 'cat']
+    }
+    ,
+    {
+        id: 3
+        , url: 'img/3.jpg'
+        , keyWords: ['funny', 'cat']
+    }
+    ,
+    {
+        id: 4
+        , url: 'img/4.jpg'
+        , keyWords: ['funny', 'cat']
+    }
+    ,
+    {
+        id: 5
+        , url: 'img/5.jpg'
+        , keyWords: ['funny', 'cat']
+    }
+]
 
 var gMeme = {
     selectedImgId: 5,
@@ -14,9 +40,13 @@ var gMeme = {
         {
             txt: 'i sometimes eat Falafel',
             size: 20,
-            color: red
+            color: 'red'
         }
     ]
 }
 
 var gKeyWordSearchCountMap = { 'funny': 12, 'cat': 16, 'baby': 2 }
+
+function getMeme(){
+    return gMeme
+}
